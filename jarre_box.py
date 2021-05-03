68e3ab126fde3710c6841572316bd058c8cbebd8
arthur <artbasket74rgmail.com>
2021-05-03 18:34:54 +0200

    deuxième essaie

--- jarre_box.py
+++ jarre_box.py
@@ -2,13 +2,23 @@
 
 from Tkinter import *
 
-#creer le fenetre
+# creer le fenetre
 window = Tk()
-window.geometry('1080x730')
+window.geometry('1080x730+400+150')
 window.title('les jarres')
+window.config(background='#a41f1f')
 
 # ajouter les écritures
-label = Label(window, text='les jarres')
-label.pack()
+bienvenue = Label(window, text='Bienvenue dans le jeu des jarres', background='#a41f1f', font=('courrier', 30))
+bienvenue.pack()
+
+regle = Label(window, text ="il y a 5 jarres, Le but est d'ouvrir une jarre sans serpent,\n a chaque bonne jarre tu gagnes"
+                          " une cle,\n au bout de trois cles tu deviens le roi du temple,\n quand tu ouvre une jarre avec "
+                          "un serpent tu perds une cle,\n il y a trois niveau,le niveau 1 et le plus simple avec un serpent "
+                          "dans une jarre et le niveau 3 le plus difficile avec 3 serpents", background='#a41f1f', font=('Arial', 15))
+regle.pack(pady=200)
+
+button_OK = Button(window, text="OK", background='black', font=('Arial', 15), fg='#a41f1f', relief='raised', cursor='circle')
+button_OK.pack(pady=0)
 
 window.mainloop()
